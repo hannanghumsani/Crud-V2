d0406c797ffd7790578fa21edf9b60a51fcebf41
ABDUL HANNAN <viveksingh@Viveks-MacBook-Air.local>
2025-02-10 20:59:29 +0530

    Change db connect function

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,25 @@
-const app = require("./index");
+const express = require("express")
+require('dotenv').config()
+const connectDB = require('./config/db')
+const userRoutes = require("./route/userRoute");
+const cors = require('cors')
 
-module.exports = app;
+
+const app = express()
+// console.log(process.env.Port);
+connectDB();
+
+app.use(cors())
+app.use(express.json());
+const Port = process.env.Port
+
+app.get("/", (req, res) => {
+    res.send("Express server is listening")
+})
+
+app.use("/user", userRoutes);
+
+
+app.listen(Port, () => {
+    console.log("listening on port 8080");
+})
