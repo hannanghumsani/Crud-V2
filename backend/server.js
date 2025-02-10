aae2d0575d4dfa04c2564758c679787f0ba458f8
ABDUL HANNAN <viveksingh@Viveks-MacBook-Air.local>
2025-02-10 22:21:16 +0530

    prod-3

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
-const express = require("express")
-require('dotenv').config()
-const connectDB = require('./config/db')
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./config/db");
 const userRoutes = require("./route/userRoute");
-const cors = require('cors')
+const cors = require("cors");
 
-
-const app = express()
-// console.log(process.env.Port);
+const app = express();
 connectDB();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-const Port = process.env.Port
 
 app.get("/", (req, res) => {
-    res.send("Express server is listening")
-})
+    res.send("Express server is running!");
+});
 
 app.use("/user", userRoutes);
 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
-app.listen(Port, () => {
-    console.log("listening on port 8080");
-})
+module.exports = app; 
